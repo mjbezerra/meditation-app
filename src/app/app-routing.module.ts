dfc9b6a9934e69251f93bc3d5fdf520c40a1d26b
mjbezerra <melaniejoyce24@gmail.com>
2024-04-01 20:01:01 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'songs',
+    loadChildren: () => import('./songs/songs.module').then( m => m.SongsPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/songs/songs.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-songs',
+  templateUrl: './songs.page.html',
+  styleUrls: ['./songs.page.scss'],
+})
+export class SongsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+    counter() {
+    const play = document.querySelector('#playBtn');
+    
+  }
+
+}
